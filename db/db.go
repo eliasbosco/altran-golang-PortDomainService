54f1f98bfbab0040ebb73d737acc35ea1fdc3e22
Elias Bosco <eliasbosco@gmail.com>
2020-12-17 10:16:11 -0400

    Database separated pkg

--- db/db.go
+++ db/db.go
@@ -1,28 +1,54 @@
 package db
 
 import (
-	"database/sql"
-	"log"
 	"../types"
 	model "./types"
+	"database/sql"
+	"errors"
+	_ "github.com/mattn/go-sqlite3"
+	"log"
+	"os"
 )
 
-type DBConn struct {
+var config *types.Config
+type Conn struct {
 	DB *sql.DB
 }
 
 //Connect - SQLite connection routine
-func Connect(_config types.Config) (*DBConn, error) {
-	db, err := sql.Open("sqlite3", _config.SQLitePath)
-	if err != nil {
+func Connect() (*Conn, error) {
+	if config == nil {
+		err := errors.New("Database configuration not informed: config is null")
 		log.Println(err)
 		return nil, err
 	}
-	defer db.Close()
 
-	//Creating table
-	sqlStmt := `
-	CREATE TABLE IF NOT EXISTS ports (
+	if _, err := os.Stat(config.SQLitePath); err != nil {
+		log.Printf("%v - creating new database file\n", err)
+		file, err := os.Create(config.SQLitePath) // Create SQLite file
+		if err != nil {
+			log.Fatal(err.Error())
+		}
+		file.Close()
+		log.Printf("Database file '%s' created", config.SQLitePath)
+	}
+
+	db, err := sql.Open("sqlite3", config.SQLitePath)
+	if err != nil {
+		log.Printf("db.Connect: %v\n", err)
+		return nil, err
+	}
+	defer db.Close()
+	log.Printf("db.Connect-db: %#v\n", db)
+
+	// create table if not exists
+	createTable(db)
+
+	return &Conn{DB: db}, nil
+}
+
+func createTable(db *sql.DB) {
+	createStudentTableSQL := `CREATE TABLE IF NOT EXISTS ports (
 		port_id char(5) PRIMARY KEY,
 		name varchar(100) NOT NULL,
 		country varchar(100) NOT NULL,
@@ -33,74 +59,84 @@ func Connect(_config types.Config) (*DBConn, error) {
 		timezone varchar(50),
 		unlocs varchar(500),
 		code varchar (100)
-	) WITHOUT ROWID;
-	CREATE INDEX [idx_ports_name] ON "ports" ([port_id]);
-	`
-	_, err = db.Exec(sqlStmt)
-	if err != nil {
-		log.Printf("%q: %s\n", err, sqlStmt)
-		return nil, err
-	}
+	) WITHOUT ROWID;` // SQL Statement for Create Table
 
-	return &DBConn{DB: db}, nil
+	log.Println("Create 'ports' table...")
+	statement, err := db.Prepare(createStudentTableSQL) // Prepare SQL Statement
+	if err != nil {
+		log.Printf("db.createTable", err.Error())
+	}
+	statement.Exec() // Execute SQL Statements
+	log.Println("'ports' table created")
 }
 
-func (db *DBConn) Exists(PortId string) (bool, error){
-	stmt, err := db.DB.Prepare("select 1 from ports where port_id = ?")
+func (c *Conn) Exists(PortId string) (bool, error){
+	log.Printf("db.Exists-db: %#v\n", c.DB)
+
+	row, err := c.DB.Query("SELECT port_id FROM ports WHERE port_id = ?", PortId)
 	if err != nil {
-		log.Fatal(err)
-	}
-	defer stmt.Close()
-	var exists int
-	err = stmt.QueryRow(PortId).Scan(&exists)
-	if err != nil {
-		log.Fatal(err)
+		log.Printf("db.Exists: %v\n", err)
 		return false, err
 	}
+	defer row.Close()
 
-	return exists != 0, nil
+	return row.Next(), nil
 }
 
 //Upset insert or update record
-func (db *DBConn) Upset(body model.Ports) (bool, error) {
-	exists, err := db.Exists(body.PortId)
-	if err != nil {
-		return false, err
-	}
+func (c *Conn) Upset(body model.Ports) (bool, error) {
+	//exists, err := c.Exists(body.PortId)
+	//if err != nil {
+	//	log.Printf("db.Upset: %v\n", err)
+	//	return false, err
+	//}
+	exists := false
 
-	tx, err := db.DB.Begin()
+	tx, err := c.DB.Begin()
 	if err != nil {
+		log.Printf("db.Upset: %v\n", err)
 		return false, err
 	}
 	if !exists {
 		stmt, err := tx.Prepare(`insert into ports 
-		(port_id,name,country,alias,regions,coordinate,province,timezone,unlocs,code) 
-		values(?, ?, ?, ?, ?, ?, ?, ?, ?, ?)`)
+		(port_id,name,country,alias,regions,coordinates,province,timezone,unlocs,code) 
+		values (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)`)
 		if err != nil {
-			log.Fatal(err)
+			log.Printf("db.Upset: %v\n", err)
+			return false, err
 		}
 		defer stmt.Close()
 
 		_, err = stmt.Exec(body.PortId, body.Name, body.Alias, body.Regions, body.Coordinates, body.Province, body.Timezone, body.Unlocs, body.Code)
 		if err != nil {
+			log.Printf("db.Upset: %v\n", err)
 			return false, err
 		}
 	} else {
 		stmt, err := tx.Prepare(`update ports set 
-		name=?,country=?,alias=?,regions=?,coordinate=?,province=?,timezone=?,unlocs=?,code=? 
+		name=?,country=?,alias=?,regions=?,coordinates=?,province=?,timezone=?,unlocs=?,code=? 
 		where port_id=?`)
 		if err != nil {
-			log.Fatal(err)
+			log.Printf("db.Upset: %v\n", err)
+			return false, err
 		}
 		defer stmt.Close()
 
 		_, err = stmt.Exec(body.Name, body.Alias, body.Regions, body.Coordinates, body.Province, body.Timezone, body.Unlocs, body.Code, body.PortId)
 		if err != nil {
+			log.Printf("db.Upset: %v\n", err)
 			return false, err
 		}
 	}
 
-	tx.Commit()
+	if err = tx.Commit(); err != nil {
+		log.Printf("db.Upset: %v\n", err)
+		return false, err
+	}
 
 	return true, nil
+}
+
+func SetConfig(_config *types.Config) {
+	config = _config
 }
--- main.go
+++ main.go
@@ -3,14 +3,56 @@ package main
 import (
 	pb "./portsgrpc"
 	"./types"
+	"context"
 	"google.golang.org/grpc"
+	"google.golang.org/grpc/channelz/service"
 	"log"
 	"net"
+	"./db"
+	dbTypes "./db/types"
+)
+
+var (
+	ports = []string{":10001", ":10002", ":10003"}
 )
 
 // server is used to implement helloworld.GreeterServer.
 type server struct {
 	pb.UnimplementedPortsDbServer
+}
+
+func (s *server) Upset(ctx context.Context, in *pb.Ports) (*pb.Response, error) {
+	log.Printf("Received port %s", in.PortId)
+
+	config := types.SetupConfig()
+	log.Printf("main.Upset-config: %v\n", config)
+
+	db.SetConfig(&config)
+	_db, err := db.Connect()
+	if err != nil {
+		log.Printf("main.Upset: %v\n", err)
+		return &pb.Response{Code: "sql", Message: err.Error()}, err
+	}
+
+	msg := "Port saved in the data base successfully."
+	if _, err := _db.Upset(dbTypes.Ports{
+		PortId:      in.PortId,
+		Name:        in.Name,
+		City:        in.City,
+		Country:     in.Country,
+		Alias:       in.Alias,
+		Regions:     in.Regions,
+		Coordinates: in.Coordinates,
+		Province:    in.Province,
+		Timezone:    in.Timezone,
+		Unlocs:      in.Unlocs,
+		Code:        in.Code,
+	}); err != nil {
+		log.Printf("main.Upset: %v\n", err)
+		msg = err.Error()
+	}
+
+	return &pb.Response{Code: "portsgrpc.Upset-1", Message: msg}, nil
 }
 
 func main() {
@@ -20,9 +62,24 @@ func main() {
 	if err != nil {
 		log.Fatalf("failed to listen: %v", err)
 	}
+	defer lis.Close()
 	s := grpc.NewServer()
-	pb.RegisterPortsDbServer(s, &server{})
-	if err := s.Serve(lis); err != nil {
-		log.Fatalf("failed to serve: %v", err)
+	service.RegisterChannelzServiceToServer(s)
+	go s.Serve(lis)
+	defer s.Stop()
+
+	/***** Start three GreeterServers(with one of them to be the slowServer). *****/
+	for i := 0; i < 3; i++ {
+		lis, err := net.Listen("tcp", ports[i])
+		if err != nil {
+			log.Fatalf("failed to listen: %v", err)
+		}
+		defer lis.Close()
+		s := grpc.NewServer()
+		pb.RegisterPortsDbServer(s, &server{})
+		go s.Serve(lis)
 	}
+
+	/***** Wait for user exiting the program *****/
+	select {}
 }
